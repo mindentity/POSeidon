0f724e8b5093119a3928f76799b65b7e0a8b4e1d
Umut Barış Öztunç <umutoztunc@gmail.com>
2019-05-12 06:15:03 +0300

    Create Settings and Currency classes

---
+++ POSeidon/Currency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSeidon
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} ({1})", Name, Symbol);
+        }
+    }
+}
---
+++ POSeidon/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace POSeidon
+{
+    public class Settings
+    {
+        public int Id { get; set; }
+        public NumberFormatInfo NumberFormat { get; set; }
+        public IList<Currency> AvailableCurrencies;
+
+        public Settings()
+        {
+            AvailableCurrencies = new List<Currency>()
+            {
+                new Currency { Name = "Dollar", Symbol = "$" },
+                new Currency { Name = "Euro", Symbol = "€" },
+                new Currency { Name = "Pound", Symbol = "£" },
+                new Currency { Name = "Ruble", Symbol = "₽" },
+                new Currency { Name = "Rupee", Symbol = "₹" },
+                new Currency { Name = "Turkish Lira", Symbol = "₺" },
+                new Currency { Name = "Yen", Symbol = "¥" }
+            };
+        }
+    }
+}
