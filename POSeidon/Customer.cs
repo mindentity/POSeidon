02035b0d7899cd55e3ca6ef5504004f1d017a6fe
Umut Barış Öztunç <umutoztunc@gmail.com>
2019-05-15 09:01:49 +0300

    Add FullName property to Customer class

--- POSeidon/Customer.cs
+++ POSeidon/Customer.cs
@@ -8,5 +8,10 @@
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
+        
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}";  }
+        }
     }
 }
